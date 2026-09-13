30f78150422afd4e0f9834d725a6325665c45098
HuiluanYie <huiluanyie@gmail.com>
2026-09-12 20:09:24 -0700

    Commit 1

--- main.cpp
+++ main.cpp
@@ -1,15 +1,40 @@
 // COMSC-210 | Lab 7 | Huiluan Yie
 
 #include <iostream>
+#include <string>
 using namespace std;
 
-//Function prototype
+const int SIZE = 5;
 
+//Function prototype
+string* reverseArray(string*);
+void displayArray(string*);
 
 int main() {
 
+    //Function call
 
     return 0;
 }
 
 //Function definition
+string* reverseArray(string* arr)
+{
+    // reverseArray() reverses the elements in the array
+    // arguments: the dynamic string array
+    // returns: a pointer to the modified array
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+    
+}
+
+void displayArray(string* arr)
+{
+    // displayArray() outputs its elements in the array
+    // arguments: the dynamic string array
+    // returns: none
+
+}
